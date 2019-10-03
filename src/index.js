ae27a5549eee22512b084d91c429e012d0d2fc3c
Romanovitch <rokos.job.ru@gmail.com>
2019-10-03 22:27:15 +0300

    feat: implement multiply() var_2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
-  return (BigInt( first ) * BigInt( second )).toString();
-}
+// //   // your solution
+// //   // return (BigInt( first ) * BigInt( second )).toString();
+  let res_arr = [];
+  second = second.split("").reverse();
+  first.split("").reverse().forEach((el_a1, i) => 
+    second.forEach((el_a2, j) => 
+      res_arr[i + j] = (res_arr[i + j]) 
+        ? res_arr[i + j] + el_a1 * el_a2 
+        : el_a1 * el_a2)
+  )
+  return res_arr
+      .map((item, index, arr) => {
+          if(arr[index + 1]){
+              arr[index + 1] = arr[index + 1] + Math.floor(arr[index] / 10);
+              return item % 10;
+          } else {
+              return item
+          }
+      })
+      .reverse().join("")
+  }
